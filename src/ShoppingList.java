5f00000fee33ce0716d300a6ca07c88f34af181e
thomas <thomasshumet22@gmail.com>
2025-09-22 15:18:01 -0700

    Shopping List Program

---
+++ src/ShoppingList.java
@@ -0,0 +1,14 @@
+public class ShoppingList {
+        public static void main(String[] args) {
+    System.out.println("Apples");
+    System.out.println("Bananas");
+    System.out.println("Milk");
+    System.out.println("Bread");
+    System.out.println("Eggs");
+    System.out.println("Cheese");
+    System.out.println("Chicken");
+    System.out.println("Rice");
+    System.out.println("Pasta");
+    System.out.println("Tomatoes");
+        }
+}
